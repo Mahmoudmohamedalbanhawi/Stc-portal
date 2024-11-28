16eb923efb0a9e18fe00e20c7baf6447e38b3964
mahmoudalbanhawi <Mahmoud@gmail.com>
2024-11-28 15:16:34 +0200

    build env folder

--- src/app/modules/core/services/home.service.ts
+++ src/app/modules/core/services/home.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Article } from '../Interface/articles';
+import {  environment } from '../../../../env/Api';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,13 +11,13 @@ export class HomeService {
 
   constructor(private http:HttpClient) { }
   FetchTrend(): Observable<Article[]>{
-    const TrendUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e103084dbd684ebda4232c01aa8ac30e`;
+    const TrendUrl = `${environment.ApiUrl}top-headlines?sources=techcrunch&apiKey=${environment.ApiKey}`;
     return this.http.get<any>(TrendUrl).pipe(
       map(data => data.articles)
     )
   }
   fetchTopics():Observable<Article[]>{
-    const Topics = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e103084dbd684ebda4232c01aa8ac30e`;
+    const Topics = `${environment.ApiUrl}top-headlines?country=us&category=business&apiKey=${environment.ApiKey}`;
     return this.http.get<any>(Topics).pipe(
       map(data => data.articles)
     )
